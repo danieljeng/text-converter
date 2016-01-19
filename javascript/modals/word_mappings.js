70b38cc40e06b4a18e2b1d17d3425461aa2a7758
Daniel Jeng <danieljeng@gmail.com>
2016-01-19 16:16:14 -0500

    Convert textarea in word mappings modal to angular datatables, add more word conversions

--- javascript/modals/word_mappings.js
+++ javascript/modals/word_mappings.js
@@ -2,11 +2,37 @@
 
 function controllerModalWordMappings ( $scope, $uibModalInstance, $http, mapWordConversions )
 {
-    $scope.stringWordMappings = JSON.stringify(mapWordConversions, null, 4);
+    $scope.wordMappings = [];
     
     $scope.cancel =
         function ()
         {
             $uibModalInstance.dismiss("cancel");
         };
+    
+    $scope.deleteWordMapping =
+        function ( index )
+        {
+            $scope.wordMappings.splice(index, 1);
+        };
+    
+    function initializeController()
+    {
+        for ( var idx in mapWordConversions )
+        {
+            $scope.wordMappings.push(
+                {
+                    unconvertedWord : idx,
+                    convertedWord   : mapWordConversions[idx],
+                }
+            );
+        }
+        
+        $scope.optionsTableWordMappings =
+        {
+            paging : false,
+        };
+    }
+    
+    initializeController();
 }
